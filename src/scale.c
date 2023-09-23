3e7478ac5f294245aef3725d101f762d4eca41e5
bratpeki <katic.peco@gmail.com>
2023-09-24 00:11:56 +0200

    Minor scales added

--- src/scale.c
+++ src/scale.c
@@ -2,20 +2,59 @@
 
 /* A variable which contains all our scales */
 
-sfscale sfScalesAll[12] = {
+sfscale sfScalesAll[48] = {
 
-	{ "cdefgab", "C Major"  },
-	{ "CDfFGAc", "C# Major" },
-	{ "deFgabC", "D Major"  },
-	{ "DfgGAcd", "D# Major" },
-	{ "eFGabCD", "E Major"  },
-	{ "fgaAcde", "F Major"  },
-	{ "FGAbCDf", "F# Major" },
-	{ "gabcdeF", "G Major"  },
-	{ "GAcCDfg", "G# Major" },
-	{ "abCdeFG", "A Major"  },
-	{ "AcdDfga", "A# Major" },
-	{ "bCDeFGA", "B Major"  }
+	{ "cdefgab", "C Major"           },
+	{ "CDfFGAc", "C# Major"          },
+	{ "deFgabC", "D Major"           },
+	{ "DfgGAcd", "D# Major"          },
+	{ "eFGabCD", "E Major"           },
+	{ "fgaAcde", "F Major"           },
+	{ "FGAbCDf", "F# Major"          },
+	{ "gabcdeF", "G Major"           },
+	{ "GAcCDfg", "G# Major"          },
+	{ "abCdeFG", "A Major"           },
+	{ "AcdDfga", "A# Major"          },
+	{ "bCDeFGA", "B Major"           },
+
+	{ "cdDfgGA", "C Minor"           },
+	{ "CDeFGab", "C# Minor"          },
+	{ "defgaAc", "D Minor"           },
+	{ "DfFGAbC", "D# Minor"          },
+	{ "eFgabcd", "E Minor"           },
+	{ "fgGAcCD", "F Minor"           },
+	{ "FGabCde", "F# Minor"          },
+	{ "gaAcdDf", "G Minor"           },
+	{ "GAbCDeF", "G# Minor"          },
+	{ "abcdefg", "A Minor"           },
+	{ "AcCDfFG", "A# Minor"          },
+	{ "bCdeFga", "B Minor"           },
+
+	{ "cdDfgGb", "C Harmonic Minor"  },
+	{ "CDeFGac", "C# Harmonic Minor" },
+	{ "defgaAC", "D Harmonic Minor"  },
+	{ "DfFGAbd", "D# Harmonic Minor" },
+	{ "eFgabcD", "E Harmonic Minor"  },
+	{ "fgGAcCe", "F Harmonic Minor"  },
+	{ "FGabCdf", "F# Harmonic Minor" },
+	{ "gaAcdDF", "G Harmonic Minor"  },
+	{ "GAbCDeg", "G# Harmonic Minor" },
+	{ "abcdefG", "A Harmonic Minor"  },
+	{ "AcCDfFa", "A# Harmonic Minor" },
+	{ "bCdeFgA", "B Harmonic Minor"  },
+
+	{ "cdDfgab", "C Melodic Minor"   },
+	{ "CDeFGAc", "C# Melodic Minor"  },
+	{ "defgabC", "D Melodic Minor"   },
+	{ "DfFGAcd", "D# Melodic Minor"  },
+	{ "eFgabCD", "E Melodic Minor"   },
+	{ "fgGAcde", "F Melodic Minor"   },
+	{ "FGabCDf", "F# Melodic Minor"  },
+	{ "gaAcdeF", "G Melodic Minor"   },
+	{ "GAbCDfg", "G# Melodic Minor"  },
+	{ "abcdeFG", "A Melodic Minor"   },
+	{ "AcCDfga", "A# Melodic Minor"  },
+	{ "bCdeFGA", "B Melodic Minor"   },
 
 };
 
