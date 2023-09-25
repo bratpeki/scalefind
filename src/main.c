f9d24232ec69482e3b45efa58bbd6bf15120c1e5
bratpeki <katic.peco@gmail.com>
2023-09-26 01:23:31 +0200

    TODO added

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@ int main(int argc, char **argv) {
 
 	if ( argc == 1 ) { cliHelp(); return EXIT_SUCCESS; }
 
+	/* TODO: Identify individual arguments, don't output as one wall of text */
+
 	for (int i = 1; i < argc; i++)
 		sfFind(argv[i]);
 	return EXIT_SUCCESS;
